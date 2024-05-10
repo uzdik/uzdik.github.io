fb5f1e89b9d42818834390b36a9c2702c4670d2c
“Үздік Білім” арнасы <89787851+uzdik@users.noreply.github.com>
2024-05-10 13:49:15 +0900

    Update youtube-embed.js

--- scripts/youtube-embed.js
+++ scripts/youtube-embed.js
@@ -8,18 +8,8 @@ $(document).ready(function() {
     $('.youtube-spoilers iframe').remove();
     
     var videoUrl = $(this).attr('href');
-    var embedUrl = getEmbedUrl(videoUrl);
+    var embedUrl = videoUrl.replace("watch?v=", "embed/");
     var iframe = $('<iframe>').attr('src', embedUrl).attr('width', '560').attr('height', '315').attr('frameborder', '0').attr('allowfullscreen', true);
     $(this).parent().append(iframe);
   });
-  
-  function getEmbedUrl(videoUrl) {
-    if (videoUrl.includes('youtube.com/watch?v=')) {
-      return videoUrl.replace("watch?v=", "embed/");
-    } else if (videoUrl.includes('youtu.be/')) {
-      var videoId = videoUrl.split('youtu.be/')[1];
-      return "https://www.youtube.com/embed/" + videoId;
-    }
-    return videoUrl; // Return original URL if format is unknown
-  }
 });
