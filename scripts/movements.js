d03ced1ebb234589527ec14ce9f69bf9d6dfa883
“Үздік Білім” арнасы <89787851+uzdik@users.noreply.github.com>
2024-05-12 17:34:10 +0900

    Update movements.js

--- scripts/movements.js
+++ scripts/movements.js
@@ -19,16 +19,19 @@ if (window.location.pathname === '/' || window.location.pathname === '/index.htm
     rotationIntervals.forEach(interval => clearInterval(interval));
   }
 
-  // Function to calculate idea position
-  function calculateIdeaPosition(index, time) {
-    const centerX = centerUser.offsetLeft + centerUser.offsetWidth / 2;
-    const centerY = centerUser.offsetTop + centerUser.offsetHeight / 2;
-    const angle = angleOffset + (index / ideas.length) * (2 * Math.PI - angleOffset * 2);
-    const x = centerX + ovalWidth / 2 * Math.cos(angle + time / 1000 + index * 0.1);
-    const y = centerY + ovalHeight / 2 * Math.sin(angle + time / 1000 + index * 0.1);
-    return { x, y };
-  }
-
+// Function to calculate idea position
+function calculateIdeaPosition(index, time) {
+  const centerX = centerUser.offsetLeft + centerUser.offsetWidth / 2;
+  const centerY = centerUser.offsetTop + centerUser.offsetHeight / 2;
+  const angle = angleOffset + (index / ideas.length) * (2 * Math.PI - angleOffset * 2);
+  // Adjust radius for distance from center
+  const radius = ovalWidth / 2 + 250; // 50 pixels further from the center
+  // Calculate x and y positions
+  const x = centerX + radius * Math.cos(angle + time / 1000 + index * 0.1);
+  const y = centerY + (ovalHeight / 2) * Math.sin(angle + time / 1000 + index * 0.1);
+  
+  return { x, y };
+}
 
   
   // Create ideas clouds rotating around the user in an oval shape
@@ -54,7 +57,7 @@ if (window.location.pathname === '/' || window.location.pathname === '/index.htm
       cloud.style.top = y + 'px';
     };
 
-    let rotationInterval = setInterval(rotateCloud, 50); // Rotate the cloud
+    let rotationInterval = setInterval(rotateCloud, 30); // Rotate the cloud
     rotationIntervals.push(rotationInterval); // Push rotation interval to the array
 
     // Stop rotation when mouse is over an idea
@@ -64,7 +67,7 @@ if (window.location.pathname === '/' || window.location.pathname === '/index.htm
 
     // Resume rotation when mouse leaves an idea
     cloud.addEventListener('mouseleave', () => {
-      rotationInterval = setInterval(rotateCloud, 50);
+      rotationInterval = setInterval(rotateCloud, 30);
     });
   });
 
