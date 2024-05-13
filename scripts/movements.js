5562305f6c1eafe51f1fab013cc643862442f016
“Үздік Білім” арнасы <89787851+uzdik@users.noreply.github.com>
2024-05-13 10:36:08 +0900

    Update movements.js

--- scripts/movements.js
+++ scripts/movements.js
@@ -25,13 +25,13 @@ if (window.location.pathname === '/' || window.location.pathname === '/index.htm
     const centerY = centerUser.offsetTop + centerUser.offsetHeight / 2 - 50;
     const angle = angleOffset + (index / ideas.length) * (2 * Math.PI - angleOffset * 2);
     // Adjust radius for distance from center
-    let radius = ovalWidth / 2 + 1000; // 200 pixels further from the center
+    let radius = ovalWidth / 2 + 300; // 200 pixels further from the center
 
     // Gradually reduce radius if close to the center
     radius -= Math.min(time / 1000, 5) * 40; // Reduce radius by up to 40 pixels per second
-    if (radius < 200) {
+    if (radius < 300) {
       // Start rotation around the center
-      radius = 200; // Maintain a minimum distance from the center
+      radius = 300; // Maintain a minimum distance from the center
     }
     
     // Calculate x and y positions
@@ -66,16 +66,16 @@ if (window.location.pathname === '/' || window.location.pathname === '/index.htm
     rotationIntervals.push(rotationInterval); // Push rotation interval to the array
 
     // Stop rotation when mouse is over this idea
-    cloud.addEventListener('mouseenter', () => {
-      clearInterval(rotationInterval);
-    });
+    //cloud.addEventListener('mouseenter', () => {
+      //clearInterval(rotationInterval);
+    //});
 
     // Resume rotation when mouse leaves this idea
-    cloud.addEventListener('mouseleave', () => {
-      rotationInterval = setInterval(rotateCloud, 50);
-      rotationIntervals.push(rotationInterval); // Push rotation interval to the array
-    });
-  });
+    //cloud.addEventListener('mouseleave', () => {
+      //rotationInterval = setInterval(rotateCloud, 50);
+      //rotationIntervals.push(rotationInterval); // Push rotation interval to the array
+    //});
+  //});
 
   // Stop rotation when mouse is over the center user
   //centerUser.addEventListener('mouseenter', () => {
