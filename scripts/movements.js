e7aab85448625a6f847ecfb04319f0e15bf125b6
“Үздік Білім” арнасы <89787851+uzdik@users.noreply.github.com>
2024-05-13 10:32:25 +0900

    Update movements.js

--- scripts/movements.js
+++ scripts/movements.js
@@ -25,7 +25,7 @@ if (window.location.pathname === '/' || window.location.pathname === '/index.htm
     const centerY = centerUser.offsetTop + centerUser.offsetHeight / 2 - 50;
     const angle = angleOffset + (index / ideas.length) * (2 * Math.PI - angleOffset * 2);
     // Adjust radius for distance from center
-    let radius = ovalWidth / 2 + 200; // 200 pixels further from the center
+    let radius = ovalWidth / 2 + 1000; // 200 pixels further from the center
 
     // Gradually reduce radius if close to the center
     radius -= Math.min(time / 1000, 5) * 40; // Reduce radius by up to 40 pixels per second
